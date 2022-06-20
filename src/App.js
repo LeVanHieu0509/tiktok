c84215d500a6f3ab91d3df205b0357ead2540852
LeVanHieu0509 <levanhieu2k1.dz@gmail.com>
2022-06-20 16:47:19 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import DefaultLayout from '~/layouts';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            //Mặc định nếu không chỉ định thì là default còn nếu chỉ định thì là route.layout
+            //const Layout = route.layout === null ? Fragment : DefaultLayout;
+            let Layout = DefaultLayout;
+
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
